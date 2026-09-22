a400bf70266e9ca25baee3f8026db1fee29e0943
FortuneNzobigeza <nzobigezaf@gmail.com>
2026-09-22 17:08:29 -0600

    Added a interactive map into profile

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,10 +1,15 @@
 import { globalStyles } from '@/styles/global';
-import { ScrollView, Text } from 'react-native';
+import { Map } from '@maplibre/maplibre-react-native';
+import { Text, View } from 'react-native';
 
 export default function ProfileScreen() {
   return (
-    <ScrollView style={globalStyles.container}>
+    <View style={globalStyles.container}>
       <Text style={globalStyles.title}>Profile</Text>
-    </ScrollView>
+      <Map mapStyle="https://tiles.openfreemap.org/styles/liberty" />     
+      
+      
+    
+    </View>
   );
 }
